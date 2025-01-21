15b7367a4b7e8e038a40ef16e453e3c2dcdf16ac
chmilkka <mishachmil228335@gmail.com>
2025-01-21 14:30:06 +0200

    added database configuration

--- UrlShortener.API/Program.cs
+++ UrlShortener.API/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
