3d90bf49c5562004a2c84bf9328f2ae332128132
chmilkka <mishachmil228335@gmail.com>
2025-01-21 21:07:06 +0200

    added custom exceptions

---
+++ UrlShortener.Domain/Exceptions/ExistingUserException.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Domain.Exceptions
+{
+    public class ExistingUserException : Exception
+    {
+        public ExistingUserException() : base("User already exists.") { }
+    }
+}
---
+++ UrlShortener.Domain/Exceptions/IncorrectCredentialsException.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Domain.Exceptions
+{
+    public class IncorrectCredentialsException : Exception
+    {
+        public IncorrectCredentialsException() : base("Incorrect email or password") { }
+    }
+}
