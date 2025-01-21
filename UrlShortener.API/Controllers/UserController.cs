a4a624a7ddaef06bc6297c9e97fdb5db1eef6a12
chmilkka <mishachmil228335@gmail.com>
2025-01-21 21:03:00 +0200

    added user controller

---
+++ UrlShortener.API/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.API.DTOs.Users;
+using UrlShortener.Application.Interfaces;
+using UrlShortener.Infrastructure.Services.JwtTokenService;
+
+namespace UrlShortener.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IJwtTokenService _jwtTokenService;
+
+        public UserController(IUserService userService, IJwtTokenService jwtTokenService)
+        {
+            _userService = userService;
+            _jwtTokenService = jwtTokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] AuthDto authDto)
+        {
+            var login = authDto.Login;
+            var password = authDto.Password;
+
+            await _userService.RegisterUserAsync(login, password);
+
+            return Ok(new { Message = "Registration successful" });
+        }
+
+        //[HttpPost("login")]
+        //public async Task<IActionResult> Login([FromBody] AuthDto authDto)
+        //{
+        //    var user = await _userService.AuthenticateUserAsync(authDto.Login, authDto.Password);
+        //    var token = _jwtTokenService.GenerateToken(user);
+
+        //    return Ok(new { Token = token });
+        //}
+    }
+}
