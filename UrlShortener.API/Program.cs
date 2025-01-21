9b1c6c4e2964f85bfef97dcb11c6e52e87695603
chmilkka <mishachmil228335@gmail.com>
2025-01-21 20:54:13 +0200

    added controllers, registered middleware and services in DI

--- UrlShortener.API/Program.cs
+++ UrlShortener.API/Program.cs
@@ -5,6 +5,12 @@ using System.Text;
 using UrlShortener.Infrastructure.Configuration;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using UrlShortener.Infrastructure.Services.JwtTokenService;
+using Microsoft.AspNetCore.Identity;
+using UrlShortener.Infrastructure.Repositories.Interfaces;
+using UrlShortener.Infrastructure.Repositories;
+using UrlShortener.Application.Interfaces;
+using UrlShortener.Application.Services;
+using UrlShortener.Application.PasswordHasher;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +22,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+
 
 var key = Encoding.ASCII.GetBytes(JwtSettings.SecretKey); 
 
@@ -42,6 +51,9 @@ builder.Services.AddAuthentication(options =>
 });
 
 builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
 var app = builder.Build();
 
@@ -51,6 +63,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
+app.UseMiddleware<UrlShortener.API.Middlewares.ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 app.Run();
