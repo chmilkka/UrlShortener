84a519ea1a09e497a4ddae4b43b2a9d68be763a1
chmilkka <mishachmil228335@gmail.com>
2025-01-21 14:28:34 +0200

    added application context and EF Core packages

---
+++ UrlShortener.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ShortUrl>()
+                .HasOne(s => s.User)
+                .WithMany(u => u.ShortUrls)
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
