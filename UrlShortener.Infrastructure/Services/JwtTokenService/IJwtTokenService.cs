d23c4e57e23d43b6b2fd79f6cfe75c0e81d819bd
chmilkka <mishachmil228335@gmail.com>
2025-01-21 21:08:00 +0200

    added jwt service and configuration

--- UrlShortener.Infrastructure/Services/JwtTokenService/IJwtTokenService.cs
+++ UrlShortener.Infrastructure/Services/JwtTokenService/IJwtTokenService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UrlShortener.Domain.Entities;
+﻿using UrlShortener.Domain.Entities;
 
 namespace UrlShortener.Infrastructure.Services.JwtTokenService
 {
--- UrlShortener.Infrastructure/Services/JwtTokenService/JwtTokenService.cs
+++ UrlShortener.Infrastructure/Services/JwtTokenService/JwtTokenService.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 using UrlShortener.Domain.Entities;
 
 namespace UrlShortener.Infrastructure.Services.JwtTokenService
