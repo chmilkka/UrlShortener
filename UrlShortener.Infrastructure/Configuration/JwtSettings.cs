d59218bac41f70c60264a136888c758b8cbf53be
chmilkka <mishachmil228335@gmail.com>
2025-01-21 21:52:01 +0200

    changed secret key

--- UrlShortener.Infrastructure/Configuration/JwtSettings.cs
+++ UrlShortener.Infrastructure/Configuration/JwtSettings.cs
@@ -2,7 +2,7 @@
 {
     public class JwtSettings
     {
-        public const string SecretKey = "top-secret-key";
+        public const string SecretKey = "megaaaaaa-super-duper-top-secret-key";
         public const string Issuer = "UrlShortenerAPI";
         public const string Audience = "UrlShortenerUser";
     }
