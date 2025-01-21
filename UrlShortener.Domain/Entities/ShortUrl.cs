f1da1f4d5fd72db2190270dc6dc211cdf78830aa
chmilkka <mishachmil228335@gmail.com>
2025-01-21 14:26:08 +0200

    added entities and enums

---
+++ UrlShortener.Domain/Entities/ShortUrl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Domain.Entities
+{
+    public class ShortUrl
+    {
+        public Guid Id { get; set; } 
+        public string OriginalUrl { get; set; } 
+        public string ShortenedUrl { get; set; }
+        public DateTime CreatedAt { get; set; } 
+        public string CreatedBy { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ UrlShortener.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } 
+        public string Login { get; set; } 
+        public string PasswordHash { get; set; } 
+        public string Role { get; set; }
+        public IEnumerable<ShortUrl> ShortUrls { get; set; }
+    }
+}
