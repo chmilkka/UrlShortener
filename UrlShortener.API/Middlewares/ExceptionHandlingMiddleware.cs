7b95f17698e52f287ef72011a2637643db37f16a
chmilkka <mishachmil228335@gmail.com>
2025-01-21 21:02:36 +0200

    added exception handler middleware

---
+++ UrlShortener.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Text.Json;
+using UrlShortener.Domain.Exceptions;
+
+namespace UrlShortener.API.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode status;
+            var stackTrace = string.Empty;
+            string message;
+
+            var exceptionType = exception.GetType();
+
+            if (exceptionType == typeof(IncorrectCredentialsException))
+            {
+                message = exception.Message;
+                status = HttpStatusCode.NotFound;
+            }    
+            else if (exceptionType == typeof(ExistingUserException))
+            {
+                message = exception.Message;
+                status = HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                message = exception.Message;
+                stackTrace = exception.StackTrace;
+                status = HttpStatusCode.InternalServerError;
+            }
+
+
+
+            var exceptionResult = JsonSerializer.Serialize(new { error = message, stackTrace });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+
+            return context.Response.WriteAsync(exceptionResult);
+        }
+    }
+}
