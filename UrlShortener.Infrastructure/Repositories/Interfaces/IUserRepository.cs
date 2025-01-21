32f815fae75c23bec5f046b8c382b95ec657fd03
chmilkka <mishachmil228335@gmail.com>
2025-01-21 21:08:16 +0200

    added user repository

---
+++ UrlShortener.Infrastructure/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Infrastructure.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByLoginAsync(string login);
+        Task CreateUser(User user);
+    }
+}
---
+++ UrlShortener.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Entities;
+using UrlShortener.Infrastructure.Repositories.Interfaces;
+
+namespace UrlShortener.Infrastructure.Repositories
+{
+    public class UserRepository(ApplicationDbContext dbContext) : IUserRepository
+    {
+        public async Task<User?> GetUserByLoginAsync(string login)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                throw new ArgumentException("Login cannot be null or empty.", nameof(login));
+            }
+
+            return await dbContext.Users.FirstOrDefaultAsync(u => u.Login == login);
+        }
+        public async Task CreateUser(User user)
+        {
+            try
+            {
+               await dbContext.AddAsync(user);
+               await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new DbUpdateException("An error occurred while saving the user.");
+            }
+        }
+    }
+}
