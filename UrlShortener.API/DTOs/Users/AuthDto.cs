2161e7f2dda4981d51158589c7072b0360f11a25
chmilkka <mishachmil228335@gmail.com>
2025-01-21 21:01:44 +0200

    cleaned usings

---
+++ UrlShortener.API/DTOs/Users/AuthDto.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortener.API.DTOs.Users
+{
+    public class AuthDto
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
--- UrlShortener.Domain/Entities/User.cs
+++ UrlShortener.Domain/Entities/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UrlShortener.Domain.Entities
+﻿namespace UrlShortener.Domain.Entities
 {
     public class User
     {
