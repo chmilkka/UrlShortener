d483239e5ec8275ee39ed55cfee2b37cc88ab57c
chmilkka <mishachmil228335@gmail.com>
2025-01-22 02:32:58 +0200

    added GetUserByIdAsync method

--- UrlShortener.Infrastructure/Repositories/Interfaces/IUserRepository.cs
+++ UrlShortener.Infrastructure/Repositories/Interfaces/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace UrlShortener.Infrastructure.Repositories.Interfaces
     public interface IUserRepository
     {
         Task<User?> GetUserByLoginAsync(string login);
+        Task<User?> GetUserByIdAsync(Guid id);
         Task CreateUser(User user);
     }
 }
--- UrlShortener.Infrastructure/Repositories/UserRepository.cs
+++ UrlShortener.Infrastructure/Repositories/UserRepository.cs
@@ -13,7 +13,14 @@ namespace UrlShortener.Infrastructure.Repositories
                 throw new ArgumentException("Login cannot be null or empty.", nameof(login));
             }
 
-            return await dbContext.Users.FirstOrDefaultAsync(u => u.Login == login);
+            try
+            {
+                return await dbContext.Users.FirstOrDefaultAsync(u => u.Login == login);
+            }
+            catch (Exception)
+            {
+                throw new KeyNotFoundException("User with this Login was not found");
+            }
         }
         public async Task CreateUser(User user)
         {
@@ -26,6 +33,23 @@ namespace UrlShortener.Infrastructure.Repositories
             {
                 throw new DbUpdateException("An error occurred while saving the user.");
             }
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid id)
+        {
+            if (string.IsNullOrWhiteSpace(id.ToString()))
+            {
+                throw new ArgumentException("ID cannot be null or empty.");
+            }
+
+            try
+            {
+                return await dbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+            }
+            catch (Exception)
+            {
+                throw new KeyNotFoundException("User with this ID was not found");
+            }          
         }
     }
 }
