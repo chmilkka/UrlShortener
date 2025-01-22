501b7043045fe75d135ffd9a67a71f5c687f46f2
chmilkka <mishachmil228335@gmail.com>
2025-01-22 02:23:54 +0200

    added new authentication configure

--- UrlShortener.API/Program.cs
+++ UrlShortener.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using UrlShortener.Application.Interfaces;
 using UrlShortener.Application.PasswordHasher;
@@ -25,34 +26,77 @@ builder.Services.AddControllers();
 
 
 
-var key = Encoding.ASCII.GetBytes(JwtSettings.SecretKey); 
+var key = Encoding.ASCII.GetBytes(JwtSettings.SecretKey);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
+builder.Services.AddAuthentication("OAuth")
+    .AddJwtBearer("OAuth", cfg =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = JwtSettings.Issuer,  
-        ValidAudience = JwtSettings.Audience,  
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ClockSkew = TimeSpan.Zero
-    };
-});
+        var secret = JwtSettings.SecretKey;
+        var myIssuer = JwtSettings.Issuer;
+        var myAudience = JwtSettings.Audience;
+        var secretBytes = Encoding.UTF8.GetBytes(secret);
+        var key = new SymmetricSecurityKey(secretBytes);
+
+        cfg.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = myIssuer,
+            ValidAudience = myAudience,
+            IssuerSigningKey = key
+        };
+
+        cfg.Events = new JwtBearerEvents
+        {
+            OnChallenge = async context =>
+            {
+                context.HandleResponse();
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+            }
+        };
+
+        cfg.Events = new JwtBearerEvents
+        {
+            OnForbidden = async context =>
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("You don't have permissions for this action");
+            }
+        };
+    });
 
 builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 builder.Services.AddScoped<IShortenedUrlService, ShortenedUrlService>();
 builder.Services.AddScoped<IShortenedUrlRepository, ShortenedUrlRepository>();
@@ -65,6 +109,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication(); 
+app.UseAuthorization();
 
 app.MapControllers();
 app.UseMiddleware<UrlShortener.API.Middlewares.ExceptionHandlingMiddleware>();
