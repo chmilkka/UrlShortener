d00dc1b0da6fa3bf387a52345517fa74a03ebb49
chmilkka <mishachmil228335@gmail.com>
2025-01-22 11:47:08 +0200

    added role handlers

---
+++ UrlShortener.Application/Policy/RoleHandlers/AdminRoleHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using UrlShortener.Application.Policy.Requirements;
+using UrlShortener.Domain.Enums;
+
+namespace UrlShortener.Application.Policy.RoleHandlers
+{
+    public class AdminRoleHandler : AuthorizationHandler<AdminRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdminRoleRequirement requirement)
+            => new RequirementsBase().VerifyRole(context, requirement, Role.Admin);
+    }
+}
---
+++ UrlShortener.Application/Policy/RoleHandlers/UserRoleHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.Application.Policy.Requirements;
+using UrlShortener.Domain.Enums;
+
+namespace UrlShortener.Application.Policy.RoleHandlers
+{
+    public class UserRoleHandler : AuthorizationHandler<UserRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserRoleRequirement requirement)
+            => new RequirementsBase().VerifyRole(context, requirement, Role.User);
+    }
+}
