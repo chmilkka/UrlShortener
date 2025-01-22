2abd17888fd071e032289036043d246eafcca038
chmilkka <mishachmil228335@gmail.com>
2025-01-23 00:16:35 +0200

    fixed routing

--- UrlShortener.Client/url-shortener-ui/src/routes/AppRouter.jsx
+++ UrlShortener.Client/url-shortener-ui/src/routes/AppRouter.jsx
@@ -5,12 +5,14 @@ import RegisterPage from '../pages/RegisterPage';
 import NotFoundPage from '../pages/NotFoundPage';
 import AboutPage from '../pages/AboutPage';
 import ShortUrlsTablePage from '../pages/ShortUrlsTablePage';
+import { useStore } from '../stores/StoresManager';
 
 const AllRoutes = () => {
     return (
         <Routes>
             <Route index replace element={<Navigate to="/urls"/>}/>
             <Route path ="/login" element={<LoginPage/>}/>
+            <Route path ="/logout" element={<Logout/>}/>
             <Route path ="/register" element={<RegisterPage/>}/> 
             <Route path ="/about" element={<AboutPage/>}/>  
             <Route path ="/urls" element={<ShortUrlsTablePage/>}/> 
@@ -19,4 +21,13 @@ const AllRoutes = () => {
     );
 };
 
+function Logout() {
+    const { userStore } = useStore();
+
+    userStore.logout();
+
+    return (
+        <Navigate to="/urls" replace={true}/>
+    )
+}
 export default AllRoutes;
