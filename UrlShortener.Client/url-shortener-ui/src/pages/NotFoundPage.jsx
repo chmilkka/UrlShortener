8b13709fcc146621d0d2305c54700bb0599ba998
chmilkka <mishachmil228335@gmail.com>
2025-01-22 20:53:23 +0200

    added not found page

---
+++ UrlShortener.Client/url-shortener-ui/src/pages/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+const NotFoundPage = () => {
+    return (
+        <h3>
+           Page not found!
+        </h3>
+    );
+};
+export default NotFoundPage;
--- UrlShortener.Client/url-shortener-ui/src/routes/AppRouter.jsx
+++ UrlShortener.Client/url-shortener-ui/src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 const AllRoutes = () => {
     return (
@@ -9,7 +10,7 @@ const AllRoutes = () => {
             <Route index replace element={<Navigate to="/login"/>}/>
             <Route path ="/login" element={<LoginPage/>}/>
             <Route path ="/register" element={<RegisterPage/>}/>         
-            <Route path='*'/>
+            <Route path='*'element={<NotFoundPage/>}/>
         </Routes>          
     );
 };
