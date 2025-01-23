dffcf43ef3e90140425839271e88b4e095b55ecc
chmilkka <mishachmil228335@gmail.com>
2025-01-23 03:37:03 +0200

    added urls page

--- UrlShortener.Client/url-shortener-ui/src/pages/ShortUrlsTablePage.jsx
+++ UrlShortener.Client/url-shortener-ui/src/pages/ShortUrlsTablePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Table,
   TableBody,
@@ -8,14 +8,80 @@ import {
   TableRow,
   Paper,
   Button,
+  TextField,
 } from "@mui/material";
-import BasicModal from "../components/modals/ViewDetailsModal";
 import ViewDetailsModal from "../components/modals/ViewDetailsModal";
+import { useStore } from "../stores/StoresManager";
+import { toast } from "react-toastify";
 
 const ShortUrlsTablePage = () => {
+
+const [originalUrl, setOriginalUrl] = useState('');
+const [originalUrlErrors, setOriginalUrlErrors] = useState('');
+
+const { urlStore } = useStore();
+const { userStore } = useStore();
+const { commonStore } = useStore();
+
+const firstRender = useRef(true);
+
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+            return;
+        }
+        validateUrl();
+    });
+
+    const validateUrl = () => {
+        if (originalUrl.length <= 0) {
+            setOriginalUrlErrors("URL should be longer");
+        } 
+        else 
+        {
+            setOriginalUrlErrors('');
+        }
+    }
+
+     const hasErrors = () =>  originalUrlErrors.length 
+            
+        const submit = async () => {
+            if (hasErrors()) {
+                return;
+            }
+            userStore.getUserIdFromJwt(commonStore.token)
+            const createUrlDto = {
+                originalUrl: originalUrl, 
+                userId: userStore.userId,
+            };
+            try {
+                await urlStore.createShortUrl(createUrlDto); 
+                toast.success("Short URL successfull created!")              
+            } catch (error) 
+            {
+                toast.error(error.response.data.error)
+            }
+                
+        }
+
     return (
     <TableContainer component={Paper} style={{ marginTop: "20px" }}>
-      <Table>
+        {userStore.isLoggedIn && (
+        <div style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
+          <TextField
+            variant="standard"
+            label="Original URL"
+            placeholder="Enter your URL"
+            value={originalUrl}
+            aria-required="true"
+            onChange={(e) => setOriginalUrl(e.target.value)}
+          />
+          <Button variant="contained" color="success" onClick={submit}>
+            Create Short URL
+          </Button>
+        </div>
+      )}
+       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Original URL</TableCell>
